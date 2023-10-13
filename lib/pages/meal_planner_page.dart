f5abbdfd9d4a85e4063102879aa5fc2636b8f54d
Emy Alwin <emy.alwin@gmail.com>
2023-10-13 14:04:48 +0200

    delar upp koden

--- lib/pages/meal_planner_page.dart
+++ lib/pages/meal_planner_page.dart
@@ -2,10 +2,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../util/recipe.dart';
 import '../util/recipe_provider.dart';
-import 'my_recipes_page.dart';
-import 'view_recipe_page.dart';
+import '../widgets/meal_planner_tile.dart';
 
 //the page, bara appbar & scaffold
 class MealPlannerPage extends StatelessWidget {
@@ -50,198 +48,25 @@ class ViewMealPlannerTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<RecipeProvider>(
-        builder: (context, value, child) => ListView.builder(
-            itemCount: value.plannerData.length,
-            itemBuilder: (context, index) {
-              final dayOfWeek = value.plannerData.keys.elementAt(index);
-              final recipe = value.plannerData[dayOfWeek];
+      builder: (context, value, child) => ListView.builder(
+        itemCount: value.plannerData.length,
+        itemBuilder: (context, index) {
+          final dayOfWeek = value.plannerData.keys.elementAt(index);
+          final recipe = value.plannerData[dayOfWeek];
 
-              return recipe != null
-                  ? MealPlannerTile(
-                      dayOfWeek: dayOfWeek,
-                      recipe: recipe,
-                      isRecipeChosen: true,
-                    )
-                  : MealPlannerTile(
-                      dayOfWeek: dayOfWeek,
-                      recipe: recipe,
-                      isRecipeChosen: false,
-                    );
-            }));
-  }
-}
-
-//hur veckodag-tile ser ut. får inte riktigt den att justera vilket recept den visas
-//tror detta behöver göras i en funktion i provider??
-class MealPlannerTile extends StatelessWidget {
-  String dayOfWeek;
-  Recipe? recipe;
-  bool isRecipeChosen;
-
-  MealPlannerTile({
-    super.key,
-    required this.dayOfWeek,
-    required this.recipe,
-    required this.isRecipeChosen,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 12, top: 12, right: 12),
-      child: Container(
-        padding: EdgeInsets.only(left: 8, right: 8, bottom: 8),
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: Colors.brown.withOpacity(0.5),
-          ),
-          borderRadius: BorderRadius.circular(30),
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.brown.withOpacity(0.2),
-              spreadRadius: 1,
-              blurRadius: 3,
-              offset: Offset(2, 3),
-            )
-          ],
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            //veckodagen
-            Stack(
-              alignment: Alignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  //rektangeln, dekoration
-                  child: Container(
-                    height: 25,
-                    width: 250,
-                    decoration: BoxDecoration(
-                      color: Color(0xffD89465),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.brown.withOpacity(0.5),
-                          spreadRadius: 1,
-                          blurRadius: 3,
-                          offset: Offset(2, 3),
-                        ),
-                      ],
-                      borderRadius: BorderRadius.circular(40),
-                    ),
-                  ),
-                ),
-                Text(
-                  dayOfWeek,
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-
-            const SizedBox(height: 3),
-
-            showRecipeOrNot(isRecipeChosen, dayOfWeek, recipe, context),
-          ],
-        ),
+          return recipe != null
+              ? MealPlannerTile(
+                  dayOfWeek: dayOfWeek,
+                  recipe: recipe,
+                  isRecipeChosen: true,
+                )
+              : MealPlannerTile(
+                  dayOfWeek: dayOfWeek,
+                  recipe: recipe,
+                  isRecipeChosen: false,
+                );
+        },
       ),
-    );
-  }
-}
-
-Widget showRecipeOrNot(bool isRecipeChosen, String dayOfWeek, Recipe? recipe,
-    BuildContext context) {
-  return isRecipeChosen
-      //om recept finns??
-      ? RecipieChosenMP(
-          recipe: recipe,
-          dayOfWeek: dayOfWeek,
-        )
-      //OM RECEPT EJ FINNS
-      : Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'You have not chosen a recipe for $dayOfWeek',
-              style: TextStyle(fontSize: 16, color: Colors.black),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (context) => MyRecipesPage()));
-              },
-              child: Text('Add recipe'),
-            ),
-          ],
-        );
-}
-
-class RecipieChosenMP extends StatelessWidget {
-  final String dayOfWeek;
-  final Recipe? recipe;
-  const RecipieChosenMP(
-      {super.key, required this.recipe, required this.dayOfWeek});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        Column(
-          //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Center(
-              child: GestureDetector(
-                child: Container(
-                  width: MediaQuery.of(context).size.height * 0.3,
-                  height: 130,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    color: Colors.brown,
-                    image: DecorationImage(
-                      image: AssetImage(recipe!.image.toString()),
-                      fit: BoxFit.cover,
-                      opacity: 0.6,
-                    ),
-                  ),
-                  child: Center(
-                    child: Text(
-                      recipe!.title,
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        backgroundColor: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return ViewRecipePage(recipe: recipe!);
-                      },
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
-        IconButton(
-          icon: Icon(Icons.close),
-          onPressed: () {
-            Provider.of<RecipeProvider>(context, listen: false)
-                .removePlannerItem(dayOfWeek, recipe!);
-          },
-        ),
-      ],
     );
   }
 }
